8cf774ff168f60ddd53ae289b237e8fdff65dd24
sahilgitt <sahilshabeer117@gmail.com>
2025-06-28 13:11:40 +0530

    console added

--- eCommerceWebapp/middlewares/authMiddleware.js
+++ eCommerceWebapp/middlewares/authMiddleware.js
@@ -1,23 +1,22 @@
-import JWT from 'jsonwebtoken'
-import userModel from '../models/userModel.js';
-
-
+import JWT from "jsonwebtoken";
+import userModel from "../models/userModel.js";
 
 export const requireSignIn = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
 
-       if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return res.status(401).send({
         success: false,
         message: "Authorization header missing or malformed",
       });
     }
 
-
-    const token = authHeader.split(" ")[1]; 
+    const token = authHeader.split(" ")[1];
 
     const decode = JWT.verify(token, process.env.JWT_SECRET);
+    console.log(decode);
+
     req.user = decode;
     next();
   } catch (error) {
@@ -31,27 +30,23 @@ export const requireSignIn = async (req, res, next) => {
 
 // admin access
 
-export const isAdmin = async (req , res , next)=>{
-    try {
-
-        const user = await userModel.findById(req.user._id)
-        if(user.role !== 1){
-            return res.status(401).send({
-                success:false ,
-                message : "Unautherized access"
-            })
-        } else{
-            next()
-        }
-        
-    } catch (error) {
-        console.log(error);
-       res.status(401).send({
-        message:"error in admin  middleware",
-        success:false,
-        error
-       })
-        
-        
+export const isAdmin = async (req, res, next) => {
+  try {
+    const user = await userModel.findById(req.user._id);
+    if (user.role !== 1) {
+      return res.status(401).send({
+        success: false,
+        message: "Unautherized access",
+      });
+    } else {
+      next();
     }
-}
+  } catch (error) {
+    console.log(error);
+    res.status(401).send({
+      message: "error in admin  middleware",
+      success: false,
+      error,
+    });
+  }
+};
