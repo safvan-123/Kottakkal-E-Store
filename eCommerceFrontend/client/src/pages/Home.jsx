cda2c34acfb923d59e58956831a4e428f0e7dd48
sahilgitt <sahilshabeer117@gmail.com>
2025-07-15 15:32:55 +0530

    Removed token and updated shop page

--- eCommerceFrontend/client/src/pages/Home.jsx
+++ eCommerceFrontend/client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Hero from "../components/homepage/Hero";
 import Categories from "../components/homepage/Categories";
 import ContactPage from "./ContactPage";
@@ -6,6 +6,10 @@ import OfferSalesPage from "../components/OfferSalesPage";
 import ServicesPage from "../components/homepage/ServicesPage";
 
 const Home = () => {
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
+
   return (
     <div>
       <Hero />
