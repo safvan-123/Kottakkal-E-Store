511629a615d892d5891fb204e5aaeeb10655d46b
sahilgitt <sahilshabeer117@gmail.com>
2025-08-08 12:43:20 +0530

    test

--- eCommerceFrontend/client/src/pages/MyOrder.jsx
+++ eCommerceFrontend/client/src/pages/MyOrder.jsx
@@ -68,7 +68,7 @@ const MyOrdersPage = () => {
     navigate(`/myorders/${orderId}`);
   };
   return (
-    <div className="max-w-4xl mx-auto p-4 sm:p-6 lg:p-10">
+    <div className="max-w-4xl mx-auto p-3 sm:p-6 lg:p-10">
       <h1 className="text-3xl font-bold text-gray-800 mb-6">My Orders</h1>
 
       {orders.length === 0 ? (
